3e1a6ca384b310c5f96317fd1051f07d17be24b4
douglasdosantos00 <douglasdossantos00@gmail.com>
2022-03-28 19:13:39 -0300

    req 9, criando rota profile

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
+
+    <div>
+      <Switch>
+        <Route path="/profile" component={ Header } />
+      </Switch>
     </div>
+
   );
 }
 
