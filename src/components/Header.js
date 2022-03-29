344cf3a52a07e0861b6460ff397af560cb877c5b
douglasdosantos00 <douglasdossantos00@gmail.com>
2022-03-28 23:11:17 -0300

    req 9, renderizaçao condicional nas paginas

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,43 @@ import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header() {
+  let pageTitle = '';
+  const checkLocationToSetTitle = () => {
+    if (window.location.pathname === '/foods') {
+      pageTitle = 'Foods';
+    }
+    if (window.location.pathname === '/drinks') {
+      pageTitle = 'Drinks';
+    }
+    if (window.location.pathname === '/profile') {
+      pageTitle = 'Profile';
+    }
+    if (window.location.pathname === '/explore') {
+      pageTitle = 'Explore';
+    }
+    if (window.location.pathname === '/done-recipes') {
+      pageTitle = 'Done Recipes';
+    }
+    if (window.location.pathname === '/favorite-recipes') {
+      pageTitle = 'Favorite Recipes';
+    }
+    if (window.location.pathname === '/explore/foods') {
+      pageTitle = 'Explore Foods';
+    }
+    if (window.location.pathname === '/explore/drinks') {
+      pageTitle = 'Explore Drinks';
+    }
+    if (window.location.pathname === '/explore/foods/ingredients') {
+      pageTitle = 'Explore Ingredients';
+    }
+    if (window.location.pathname === '/explore/drinks/ingredients') {
+      pageTitle = 'Explore Ingredients';
+    }
+    if (window.location.pathname === '/explore/foods/nationalities') {
+      pageTitle = 'Explore Nationalities';
+    }
+  };
+  checkLocationToSetTitle();
   return (
     <header>
       <Link to="/profile">
@@ -15,17 +52,21 @@ function Header() {
           />
         </button>
       </Link>
-      <h1 data-testid="page-title">Foods</h1>
-      <button
-        type="button"
-        // onClick={ handleClick }
-      >
-        <img
-          src={ searchIcon }
-          alt="profile"
-          data-testid="search-top-btn"
-        />
-      </button>
+      <h1 data-testid="page-title">{ pageTitle }</h1>
+      {
+        (pageTitle === 'Foods' || pageTitle === 'Explore Nationalities'
+         || pageTitle === 'Drinks')
+        && (
+          <button
+            type="button"
+          >
+            <img
+              src={ searchIcon }
+              alt="profile"
+              data-testid="search-top-btn"
+            />
+          </button>)
+      }
 
     </header>
   );
