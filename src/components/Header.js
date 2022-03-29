f71c4ffc4cc7bd77de70aebd732c56562560cfa6
jessica <jessicathaynnara@gmail.com>
2022-03-29 17:32:14 -0300

    Armazena o filtro escolhido e o valor digitado no input para fazer a busca a API

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import RecipesContext from '../context/RecipesContext';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 import './header.css';
 
 function Header() {
   const [input, setInput] = useState('false');
+  const [filter, setFilter] = useState('');
+  const [value, setValue] = useState('');
+  const { getFoods } = useContext(RecipesContext);
   let pageTitle = '';
 
   const checkLocationToSetTitle = () => {
@@ -52,6 +56,11 @@ function Header() {
       setInput('true');
     }
   };
+
+  const handleRadio = ({ target }) => {
+    setFilter(target.name);
+  };
+
   return (
     <header className="header-container">
       <div className="header-icons">
@@ -67,73 +76,78 @@ function Header() {
         <h1 data-testid="page-title">{ pageTitle }</h1>
         {
           (pageTitle === 'Foods' || pageTitle === 'Explore Nationalities'
-         || pageTitle === 'Drinks')
-        && (
-          <button
-            type="button"
-            onClick={ handleClick }
-          >
-            <img
-              src={ searchIcon }
-              alt="profile"
-              data-testid="search-top-btn"
-            />
-          </button>)
+|| pageTitle === 'Drinks')
+&& (
+  <button
+    type="button"
+    onClick={ handleClick }
+  >
+    <img
+      src={ searchIcon }
+      alt="profile"
+      data-testid="search-top-btn"
+    />
+  </button>)
         }
       </div>
       <div className="header-input">
         {
           input === 'true'
-        && <input
-          data-testid="search-input"
-          type="text"
-          placeholder="search recipe"
-        />
+&& <input
+  data-testid="search-input"
+  type="text"
+  placeholder="search recipe"
+  onChange={ ({ target }) => setValue(target.value) }
+/>
         }
       </div>
       {
         pageTitle === 'Foods'
-        && (
-          <>
-            {' '}
-            <div className="header-radio">
-              <label htmlFor="ingredient">
-                <input
-                  data-testid="ingredient-search-radio"
-                  type="radio"
-                  name="ingredient"
-                />
-                Ingredient
-              </label>
-              <label htmlFor="name">
-                <input
-                  data-testid="name-search-radio"
-                  type="radio"
-                  name="name"
-                />
-                Name
-              </label>
-              <label htmlFor="first-letter">
-                <input
-                  data-testid="first-letter-search-radio"
-                  type="radio"
-                  name="first-letter"
-                />
-                First Letter
-              </label>
+&& (
+  <>
+    {' '}
+    <div className="header-radio">
+      <label htmlFor="ingredient">
+        <input
+          data-testid="ingredient-search-radio"
+          type="radio"
+          name="ingredient"
+          onClick={ handleRadio }
+        />
+        Ingredient
+      </label>
+      <label htmlFor="name">
+        <input
+          data-testid="name-search-radio"
+          type="radio"
+          name="name"
+          onClick={ handleRadio }
+        />
+        Name
+      </label>
+      <label htmlFor="first-letter">
+        <input
+          data-testid="first-letter-search-radio"
+          type="radio"
+          name="first-letter"
+          onClick={ handleRadio }
+        />
+        First Letter
+      </label>
 
-            </div>
-            <div>
-              <button
-                type="button"
-                data-testid="exec-search-btn"
-              >
-                Search
-              </button>
-            </div>
-          </>
+    </div>
+    <div>
+      <button
+        type="button"
+        data-testid="exec-search-btn"
+        onClick={ () => getFoods(filter, value) }
+      >
+        Search
+      </button>
+    </div>
+  </>
 
-        )
+)
       }
 
     </header>
