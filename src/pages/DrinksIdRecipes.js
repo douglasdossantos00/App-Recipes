8222d391988a3198a37be6f4fc762ebad1868d66
douglasdosantos00 <douglasdossantos00@gmail.com>
2022-03-28 23:45:19 -0300

    req 10, criaçao das paginas

---
+++ src/pages/DrinksIdRecipes.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function DrinksIdRecipes() {
+  return (
+    <p>DrinksIdRecipes</p>
+  );
+}
+export default DrinksIdRecipes;
---
+++ src/pages/Explore.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Header from '../components/Header';
+
+function Explore() {
+  return (
+    <Header />
+  );
+}
+export default Explore;
