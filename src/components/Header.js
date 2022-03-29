f1c12377db281d088c4eabe28c6170da8bcb082c
douglasdosantos00 <douglasdossantos00@gmail.com>
2022-03-29 15:24:34 -0300

    css para header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
+import './header.css';
 
 function Header() {
   const [input, setInput] = useState('false');
@@ -52,19 +53,20 @@ function Header() {
     }
   };
   return (
-    <header>
-      <Link to="/profile">
-        <button type="button">
-          <img
-            src={ profileIcon }
-            alt="profile"
-            data-testid="profile-top-btn"
-          />
-        </button>
-      </Link>
-      <h1 data-testid="page-title">{ pageTitle }</h1>
-      {
-        (pageTitle === 'Foods' || pageTitle === 'Explore Nationalities'
+    <header className="header-container">
+      <div className="header-icons">
+        <Link to="/profile">
+          <button type="button">
+            <img
+              src={ profileIcon }
+              alt="profile"
+              data-testid="profile-top-btn"
+            />
+          </button>
+        </Link>
+        <h1 data-testid="page-title">{ pageTitle }</h1>
+        {
+          (pageTitle === 'Foods' || pageTitle === 'Explore Nationalities'
          || pageTitle === 'Drinks')
         && (
           <button
@@ -77,14 +79,61 @@ function Header() {
               data-testid="search-top-btn"
             />
           </button>)
-      }
-      {
-        input === 'true'
+        }
+      </div>
+      <div className="header-input">
+        {
+          input === 'true'
         && <input
           data-testid="search-input"
           type="text"
           placeholder="search recipe"
         />
+        }
+      </div>
+      {
+        pageTitle === 'Foods'
+        && (
+          <>
+            {' '}
+            <div className="header-radio">
+              <label htmlFor="ingredient">
+                <input
+                  data-testid="ingredient-search-radio"
+                  type="radio"
+                  name="ingredient"
+                />
+                Ingredient
+              </label>
+              <label htmlFor="name">
+                <input
+                  data-testid="name-search-radio"
+                  type="radio"
+                  name="name"
+                />
+                Name
+              </label>
+              <label htmlFor="first-letter">
+                <input
+                  data-testid="first-letter-search-radio"
+                  type="radio"
+                  name="first-letter"
+                />
+                First Letter
+              </label>
+
+            </div>
+            <div>
+              <button
+                type="button"
+                data-testid="exec-search-btn"
+              >
+                Search
+              </button>
+            </div>
+          </>
+
+        )
       }
 
     </header>
