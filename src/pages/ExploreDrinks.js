6e803149ca91c827430a82bf82efd719c675c138
Geovanna Carolline <geovanna.carolline.gcs@gmail.com>
2022-03-31 16:54:05 -0400

    Requisito 70 ao 73 - configura rotas

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -1,13 +1,42 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-function ExploreDrinks() {
+function ExploreDrinks({ history }) {
+  const handleClick = ({ target: { id } }) => {
+    if (id === 'explore-by-ingredient') {
+      history.push('./drinks/ingredients');
+    }
+  };
   return (
     <>
       <Header pageTitle="Explore Drinks" />
+      <button
+        data-testid="explore-by-ingredient"
+        id="explore-by-ingredient"
+        type="button"
+        onClick={ handleClick }
+      >
+        By Ingredient
+      </button>
+      <button
+        data-testid="explore-surprise"
+        id="explore-surprise"
+        type="button"
+        // onClick={ handleClick }
+      >
+        Surprise me!
+      </button>
       <Footer />
     </>
   );
 }
+
+ExploreDrinks.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
 export default ExploreDrinks;
