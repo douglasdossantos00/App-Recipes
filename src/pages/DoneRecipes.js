6cf955ce3155066e974b333c7a4315f022d597c5
douglasdosantos00 <douglasdossantos00@gmail.com>
2022-04-02 20:49:10 -0300

    criação dos filktros e redirecionamento para tela de detalhes

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -4,6 +4,23 @@ import Header from '../components/Header';
 
 function DoneRecipes() {
   const [recipes, setRecipes] = useState([]);
+  const [filterRecipes, setFilterRecipes] = useState();
+
+  const filter = filterRecipes || recipes;
+
+  const handleClick = ({ target }) => {
+    if (target.name === 'all') {
+      return setFilterRecipes();
+    }
+    if (target.name === 'food') {
+      const foods = recipes.filter(({ type }) => type === 'food');
+      return setFilterRecipes(foods);
+    }
+    if (target.name === 'drink') {
+      const drinks = recipes.filter(({ type }) => type === 'drink');
+      return setFilterRecipes(drinks);
+    }
+  };
 
   useEffect(() => {
     const doneRecipes = JSON
@@ -18,23 +35,32 @@ function DoneRecipes() {
       <button
         data-testid="filter-by-all-btn"
         type="button"
+        onClick={ handleClick }
+        name="all"
       >
         All
       </button>
       <button
         data-testid="filter-by-food-btn"
         type="button"
+        onClick={ handleClick }
+        name="food"
       >
+
         Food
       </button>
       <button
         data-testid="filter-by-drink-btn"
         type="button"
+        onClick={ handleClick }
+        name="drink"
       >
+
         Drinks
       </button>
       {
-        recipes.map((elem, index) => (
+
+        filter.map((elem, index) => (
           <CardDoneRecipes
             key={ elem.name }
             index={ index }
@@ -43,7 +69,7 @@ function DoneRecipes() {
             tags={ elem.tags }
             date={ elem.doneDate }
             category={ elem.category }
-            nationality={ elem.nationality }
+            nationality={ elem.nationality || elem.alcoholicOrNot }
             id={ elem.id }
             page={ elem.type }
           />
