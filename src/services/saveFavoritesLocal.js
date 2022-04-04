b75190a2609367a54b2971ed6de0222f692e5e2e
douglasdosantos00 <douglasdossantos00@gmail.com>
2022-04-04 16:15:08 -0300

    refatorção para adequar bug favoritar no localStorage

--- src/services/saveFavoritesLocal.js
+++ src/services/saveFavoritesLocal.js
@@ -9,13 +9,13 @@ const saveFavorites = (recipe) => {
     image: recipe.strMealThumb || recipe.strDrinkThumb,
   };
   const favorites = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
-  const NameRecipe = Object.values(object)[1];
-  const verify = favorites.find((favorite) => Object.values(favorite)[1] === NameRecipe);
+  const NameRecipe = Object.values(object)[0];
+  const verify = favorites.find((favorite) => Object.values(favorite)[0] === NameRecipe);
   if (!verify) {
     localStorage.setItem('favoriteRecipes', JSON.stringify([...favorites, object]));
   } else {
     const newFavorites = favorites
-      .filter((item) => Object.values(item)[1] !== NameRecipe);
+      .filter((item) => Object.values(item)[0] !== NameRecipe);
     localStorage.setItem('favoriteRecipes', JSON.stringify(newFavorites));
   }
 
