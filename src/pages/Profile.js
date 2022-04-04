78b16c5746b6274f891ca8df374266abbe3cbf56
obeaune <linecora86@gmail.com>
2022-04-04 12:57:50 -0300

    Requisitos 85 e 86 - redirecionar o usuário para as rotas específicas definidas nos botões

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -14,30 +15,40 @@ function Profile() {
     <>
       <Header pageTitle="Profile" />
       <div>
+
         <p data-testid="profile-email">
           { email }
         </p>
-        <button
-          type="button"
-          data-testid="profile-done-btn"
-        >
-          Done Recipes
-        </button>
-        <button
-          type="button"
-          data-testid="profile-favorite-btn"
-        >
-          Favorite Recipes
-        </button>
+
+        <Link to="/done-recipes">
+          <button
+            type="button"
+            data-testid="profile-done-btn"
+          >
+            Done Recipes
+          </button>
+        </Link>
+
+        <Link to="/favorite-recipes">
+          <button
+            type="button"
+            data-testid="profile-favorite-btn"
+          >
+            Favorite Recipes
+          </button>
+        </Link>
+
         <button
           type="button"
           data-testid="profile-logout-btn"
         >
           Logout
         </button>
+
       </div>
       <Footer />
     </>
   );
 }
+
 export default Profile;
