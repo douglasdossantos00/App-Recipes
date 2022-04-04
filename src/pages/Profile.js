806f92e885250f5d26140403e4f9f5a32c8f78e0
obeaune <linecora86@gmail.com>
2022-04-04 12:08:06 -0300

    Implementando o data-testid do email e de todos os botões

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -6,6 +6,29 @@ function Profile() {
   return (
     <>
       <Header pageTitle="Profile" />
+      <div>
+        <p data-testid="profile-email">
+          Futuro Email
+        </p>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+        >
+          Done Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Favorite Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+        >
+          Logout
+        </button>
+      </div>
       <Footer />
     </>
   );
